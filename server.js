6c963ab72bc11cbf87c0c8372cfe9186e142c96f
Ryan Tanay <ryantanay@newevolution.org>
2016-08-25 16:26:43 -0500

    Scaffolded out basic server serving nothing, added eslint config to package.json

---
+++ server.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const express = require('express');
+const favicon = require('serve-favicon'); // eslint-disable-line no-unused-vars
+const app = express();
+
+app.set('view engine', 'pug');
+
+const path = require('path')
+app.use(require('node-sass-middleware')({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  indentedsyntax: true,
+  sourcemap: true
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 3000; // eslint-disable-line no-magic-numbers
+app.listen(PORT, () => {
+  console.log(`Server started, listeing on ${PORT}`) // eslint-disable-line no-console
+})
+
+module.exports = app;
