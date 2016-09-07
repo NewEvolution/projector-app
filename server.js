e4bcc1f44b07cd4373b184de09d9ac788574ac46
Ryan Tanay <ryantanay@newevolution.org>
2016-09-07 10:26:57 -0500

    Modified location of Sass & compilation settings

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.set('view engine', 'pug');
 
 const path = require('path')
 app.use(require('node-sass-middleware')({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
+  src: path.join(__dirname, '/sass'),
+  dest: path.join(__dirname, '/public/styles'),
   indentedsyntax: true,
   sourcemap: true
 }));
