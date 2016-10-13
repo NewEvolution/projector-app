03936ad893f8d0c72f2b3a36f84135959c27bab7
Ryan Tanay <ryantanay@newevolution.org>
2016-10-13 09:07:30 -0500

    Pluralization, I will get you yet.

--- public/script.js
+++ public/script.js
@@ -4,9 +4,9 @@ const osx_button = document.getElementById('osx');
 const win_button = document.getElementById('win');
 
 win_button.addEventListener('click', () => {
-  window.open('download/windows_projector_app.exe.zip');
+  window.open('downloads/windows_projector_app.exe.zip');
 });
 
 osx_button.addEventListener('click', () => {
-  window.open('download/mac_projector_app.dmg.zip');
+  window.open('downloads/mac_projector_app.dmg.zip');
 });
